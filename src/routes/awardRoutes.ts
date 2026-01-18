3d7d565ebd4bf3b731ca041fb3943879ae2074d5
joaowalter <joao.walter07@gmail.com>
2026-01-18 12:10:31 -0300

    fix to use only /award-intervals route

--- src/routes/awardRoutes.ts
+++ src/routes/awardRoutes.ts
@@ -4,7 +4,7 @@ import { AwardController } from '../controllers/AwardController';
 const router = Router();
 
 export function createAwardRoutes(awardController: AwardController): Router {
-    router.get('/intervals', (req: Request, res: Response) => {
+    router.get('/award-intervals', (req: Request, res: Response) => {
         awardController.getAwardIntervals(req, res);
     });
     return router;
